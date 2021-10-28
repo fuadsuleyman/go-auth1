c040c990f951db63465fa3468c23a52c9b65eee3
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-28 14:33:27 +0400

    only one ilqars api allow

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -70,7 +70,7 @@ func (h *Handler) InitRoutes() *gin.Engine{
 
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://172.16.50.158:8080/, http://172.16.50.158:8080, http://localhost:8080, http://localhost:8080")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://172.16.50.158:8080")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
