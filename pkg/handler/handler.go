42f2ff3b1dcda2ca798a7c76bf7e84b120419b04
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-28 13:07:48 +0400

    change middleware for cors

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -40,15 +40,10 @@ func (h *Handler) InitRoutes() *gin.Engine{
 
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
-        c.Header("Vary", "Accept,Orign")
-        c.Header("Allow", "POST,OPTION")
-        c.Header("Referrer-Policy", "same-orign")
-        c.Header("Connection", "keep-alive")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
@@ -58,3 +53,25 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Next()
     }
 }
+
+
+// func CORSMiddleware() gin.HandlerFunc {
+//     return func(c *gin.Context) {
+
+//         c.Header("Access-Control-Allow-Origin", "*")
+//         c.Header("Access-Control-Allow-Credentials", "true")
+//         c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+//         c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+//         c.Header("Vary", "Accept,Orign")
+//         c.Header("Allow", "POST,OPTION")
+//         c.Header("Referrer-Policy", "same-orign")
+//         c.Header("Connection", "keep-alive")
+
+//         if c.Request.Method == "OPTIONS" {
+//             c.AbortWithStatus(204)
+//             return
+//         }
+
+//         c.Next()
+//     }
+// }
